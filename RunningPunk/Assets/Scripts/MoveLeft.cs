9987e1ee885138e4aa35d8b0a95e80fdc05c5ccc
Uporant228 <danila228337228@gmail.com>
2021-04-23 14:19:10 +0300

    v1
    no score

---
+++ RunningPunk/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 1;
+    [SerializeField]
+    private float _bounds = -15;
+    private PlayerController _playerController;
+    private void Start()
+    {
+        _playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+    void Update()
+    {
+        if (!_playerController.GetIsGameOver())
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * _speed);
+        }
+        if (gameObject.transform.position.x < _bounds && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ RunningPunk/Assets/Scripts/RepeatBackGround.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackGround : MonoBehaviour
+{
+    private Vector3 _startPosition;
+    private float _repeatSize;
+    
+    void Start()
+    {
+        _startPosition = transform.position;
+        _repeatSize = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    void Update()
+    {
+        if (transform.position.x < _startPosition.x -_repeatSize)
+        {
+            transform.position = _startPosition;
+        }
+    }
+}
